54a341185990f827347a9db141293f46b5780791
tndus3042 <sychoi1101@naver.com>
2025-04-03 16:01:24 +0900

    20250403 강의

--- src/main/java/com/estsoft/demo/controller/MemberController.java
+++ src/main/java/com/estsoft/demo/controller/MemberController.java
@@ -4,8 +4,7 @@ import com.estsoft.demo.repository.Member;
 import com.estsoft.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +17,28 @@ public class MemberController {
     @GetMapping("/members")
     public List<Member> showMembers() {
         return memberService.getMemberAll();
+    }
+
+    @ResponseBody
+    @PostMapping("/members")
+    public Member saveMember(@RequestBody Member member) {
+        return memberService.insertMember(member);
+    }
+
+    // GET /members/{id} 회원 단건 조회
+    @ResponseBody
+    @GetMapping("/members/{id}")
+    public Member selectMemberById(@PathVariable Long id) {
+        // findById(id) select * from where id = #id
+        return memberService.selectMemberById(id);
+    }
+    
+    // DELETE /member/{id} -> 단건 삭제
+    @ResponseBody
+    @DeleteMapping("/members/{id}")
+    public String deleteMemberById(@PathVariable Long id) {
+        memberService.deleteMemberById(id);
+        return "삭제 성공";
     }
 
     @GetMapping("/hi")
--- src/main/java/com/estsoft/demo/service/MemberService.java
+++ src/main/java/com/estsoft/demo/service/MemberService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor // final 필드만
@@ -16,5 +17,22 @@ public class MemberService {
     // getMemberAll 멤버 정보를 모두 조회
     public List<Member> getMemberAll() {
         return memberRepository.findAll();
+    }
+
+    // member 테이블에 insert 쿼리
+    public Member insertMember(Member member) {
+        Member savedMember = memberRepository.save(member);
+        return savedMember;
+    }
+
+    // 회원 단건 조회 메서드 추가
+    public Member selectMemberById(Long id) {
+        Optional<Member> optMember = memberRepository.findById(id);
+        return optMember.orElse(new Member());
+    }
+
+    // 회원 삭제 추가
+    public void deleteMemberById(Long id) {
+        memberRepository.deleteById(id);
     }
 }
