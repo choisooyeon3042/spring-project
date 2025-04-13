4e89095c0371134f234fdf2595c70e6f1d73e2b7
tndus3042 <sychoi1101@naver.com>
2025-04-13 17:33:49 +0900

    0413 gitignore.

--- src/main/java/com/estsoft/demo/configuration/MyConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.estsoft.demo.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.estsoft")
-public class MyConfiguration {
-}
